dd555f9bc582de779d40e1534650be65040d7033
Audrey Chow <audreykchow@gmail.com>
2021-01-30 14:19:39 -0800

    Debugging GetIntersections
    GetIntersections is not working.

--- public/js/paper.js
+++ public/js/paper.js
@@ -1,5 +1,9 @@
+paper.install(window);
+
 var lefthand = './img/lefthand.svg'
 var righthand = './img/righthand.svg'
+var theirhand
+var yourhand
 
 project.importSVG(righthand, function(item){
     theirhand = item
@@ -17,4 +21,24 @@ project.importSVG(lefthand, function(item){
 
 function onMouseMove(event) {
 	yourhand.position = event.point;
+    
+    for (var i = 0; i < theirhand.children.length; i++) {
+        for (var j = 0; j < yourhand.children.length; j++) {
+            showIntersections(yourhand.children[j], theirhand.children[i])
+        }
+    }
+}
+
+function showIntersections(path1, path2) {
+    var intersections = path1.getIntersections(path2);
+    if (intersections != null) {
+        console.log("hello");
+    }
+    for (var i = 0; i < intersections.length; i++) {
+        new Path.Circle({
+            center: intersections[i].point,
+            radius: 5,
+            fillColor: '#009dec'
+        }).removeOnMove();
+    }
 }
