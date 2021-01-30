f1b6ffa8611f0c3569c0bdc23a47f702e65643a6
Ethan <ethanmahc@gmail.com>
2021-01-30 14:21:59 -0800

    paperCanvas updated size

--- public/js/hands.js
+++ public/js/hands.js
@@ -18,13 +18,12 @@ export class Hands{
         // Create an empty project and a view for the canvas:
         paper.install(window);
 
-            console.log("window loaded");
-            // setup loads an id for the canvas
-            paper.setup('paperCanvas');
-            var myCircle = new Path.Circle(new Point(100, 70), 50);
-            myCircle.fillColor = 'black';
+        console.log("window loaded");
+        // setup loads an id for the canvas
+        paper.setup('paperCanvas');
+        var myCircle = new Path.Circle(new Point(200, 70), 20);
+        myCircle.fillColor = 'black';
 
-        
     }
     
     sendToSocket(area, xMiddle, yMiddle){
