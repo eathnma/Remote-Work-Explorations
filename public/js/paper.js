99f3c1da14c57f5a410321fcb986e76da437da8b
Audrey Chow <audreykchow@gmail.com>
2021-02-13 15:20:56 -0800

    New Hands

--- public/js/paper.js
+++ public/js/paper.js
@@ -1,23 +1,74 @@
 paper.install(window);
 
+// Adapted from the following Processing example:
+// http://processing.org/learning/topics/follow3.html
+
 var lefthand = './img/lefthand.svg'; 
 var righthand = './img/righthand.svg'; 
-var pathData1 = 'M63.25,50.23c-2-3.13-16.49-2.39-17.77-.2l8-13.67c2.2-3.76,9.1-11.54,9.28-15.89C63.44,3.41,43.84,32,42.17,30.82c-3.8-2.59,17.19-24.38,9.14-29.27-8.73-5.3-15.73,25.69-19.74,23.92-3.47-1.54,11.8-23,3.11-25.35C26.86-2,23.25,23.58,19.47,22.4c.16,0,2.33-31.07-7.77-12.12C6.76,19.55,2.62,33.52.52,43.85c-2.65,13,5.12,23.19,17.7,26.92,9.94,2.95,19.79.74,28.53-4.42C50.74,64,66,56.08,63.25,50.23ZM14.87,41.54A1.79,1.79,0,0,1,14.78,38a1.84,1.84,0,0,1,2,1.16,1.88,1.88,0,0,1-1.91,2.41Zm12,6.15a6.08,6.08,0,0,1-3.57,1.15,6.3,6.3,0,0,1-2.3-.43,5.32,5.32,0,0,1-3.47-4.16.5.5,0,1,1,1-.11,4.32,4.32,0,0,0,2.83,3.33,5.17,5.17,0,0,0,4.85-.57.5.5,0,1,1,.69.73l-.07.05Zm6.63.85A1.79,1.79,0,0,1,33.41,45a1.84,1.84,0,0,1,2,1.16,1.88,1.88,0,0,1-1.92,2.41Z';
-var pathData2 = 'M16.83,66.35c8.74,5.16,18.59,7.37,28.53,4.42C57.94,67,65.71,56.85,63.06,43.85,61,33.52,56.82,19.55,51.88,10.28,41.78-8.67,44,22.4,44.11,22.4,40.33,23.58,36.72-2,28.9.12,20.21,2.47,35.48,23.93,32,25.47,28,27.24,21-3.75,12.27,1.55,4.22,6.44,25.21,28.23,21.41,30.82,19.74,32,.14,3.41.82,20.47,1,24.82,7.9,32.6,10.1,36.36L18.1,50c-1.28-2.19-15.77-2.93-17.77.2C-2.42,56.08,12.84,64,16.83,66.35ZM48.07,41.47a1.88,1.88,0,0,1-1.27-2.33h0a1.84,1.84,0,0,1,2-1.16,1.79,1.79,0,0,1-.09,3.57A1.88,1.88,0,0,1,48.07,41.47ZM36.75,47.68l-.07,0a.5.5,0,1,1,.69-.73,5.17,5.17,0,0,0,4.85.57,4.32,4.32,0,0,0,2.83-3.33.5.5,0,0,1,1,.11h0a5.32,5.32,0,0,1-3.47,4.16,6.3,6.3,0,0,1-2.3.43,6.08,6.08,0,0,1-3.57-1.15Zm-6.66.86a1.88,1.88,0,0,1-1.92-2.41,1.84,1.84,0,0,1,2-1.16,1.79,1.79,0,0,1-.09,3.57Z';
+var pathData2 = 'M1078.5,636.5c0,0,7-18,3-37s-13-20-15-52s-6-60-11-85s-4-44-2-58s3-35,6-48s4-20,12-20s9.7,4.2,15,13c3,5,10,25,8,38s-8,17-8,17 M1093.5,392.5c0,18.4,3.2,32.9,8,45c4,10,6.9,15.7,12,14 M1213.5,681.5c14-26,39-38,65-65s41-76,47-99s15-53,24-76s17-38,23-57s3-17,4-24s0-10-7-12s-12,6-16,12s-15,32-24,42s-13,17-24,36s-15,32-13,25s2-5,5-17s12-33,20-57c7.4-22.2,14-35,24-59s11-29,13-42s3-20-6-23s-13,4-13,4s-5,0-15,15c-8.5,12.7-26,47-35,69c-6.5,15.8-29,61-29,61c-6,13-7,14-3,5c0,0,11-26,20-46s15-42,21-63s13-35,18-53s3-24,3-24s1-9-8-13s-16,6-16,6s-5,4-14,22s-20,42-30,62c-9,18-18,43-27,65s-13,27-12,21s1-11,4-28s7-28,12-46s12-42,17-57s8-28,8-40s-14-16-22-8c-12,8-25,39-37,70s-17,45-21,59s-16,39-22,58c-4.1,13-14.5,24.4-26,38c-11.6,13.8-15.2,18.9-29,20 M1058.4,362c-1.1,5.5,5.1,10.5,11.1,10.5s10-3,12-6s2-18,0.1-24.8 M1227.5,217.5c-6.8,6.5-11,22-7,27s10.1,7.3,17,5c6-2,9-10,12-21.7 M1292.3,225.5c-4.8,10-8.8,19-6.8,24s7,9,13,9s9-1,11-6s6.5-17.1,5.7-22.6 M1336.4,268.3c-4.9,7.2-10.9,17.2-10.9,23.2s3.9,12,12,12c6,0,9.8-1.1,14-11c3-7,5.7-15.9,3.3-21.9 M1358.5,353.5c-6.7,8.9-8,18-5,22c2.9,3.9,11.5,7,16,3c3.2-2.8,6-9,7.2-20 M1187.5,328.5c7-2,16,1,19,5 M1188.5,337.5c4,4,11,3,11,3 M1261.5,359.5c-1-3-5-10-16-10M1245.5,361.5c3,4,8,4,8,4 M1302.5,391.5c-5-6-11-10-19-9 M1332.5,445.5c0,0-3-7-12-9 M1052.5,418.5c7.3,2.4,17-1,17-1M1163.5,459.5c0,0,4-10,7-19 M1201.5,492.5c0,0,2-5,5-12c3.9-9.1,7-15,12-33 M1255.5,477.5c-4,10-6,18-6,18';
+var pathData1 = 'M835.5,674.5c0,0-8.1-17-9-35c-2-23,3-35,3-35 M776.5,624.5c21,0,31.5,1.5,43-10c10-10,31.7-31.3,43-42c17-16,27-27,33-46s26-57,25-81s0-33,2-49s-4-23-15-21s-29,32-33,44s-1,29-1,29s-11.9,10.1-18,21c-9,16-9,31-9,31 M877.5,411.5c-5-49.6-5-50.8-6-83c-1-32-4-45-4-74s-2-60-22-59s-21,36-21,46s2,11,2,15s-4,21-2,34s0,17-1,23s-3,21-2,39s2,24-5,23 M829.5,210.5c0,0,2-25-13-30s-22.5,11.1-27,36c-4,22,0,29,0,34s-8,23-8,56c0,22-5,24-5,42s6,27-14,27 M790.8,209.8c0,0-0.3-17.3-14.3-17.3c-15,0-22,26-25,41s3,26.3,0,33c-4,9-9.9,23-9,41c1,21-5,12.9-5,39c0,24,2.7,32.4-10,38c-9,4-12,5-12,5 M750.9,247.9c0,0-13.4-2.4-20.4,5.6s-19,33-21,45c-1.7,9.9,0,19-5,26s-18,39-19,55s1.8,9.4-6,25c-6,12-21,51-25,95s-4.8,86.5,12,102c13,12,17,13,17,13 M683.5,674.5c0,0-2-4-7-26s-3-27-11.6-48.6 M717.5,311.5c6,3,14,3,14,3 M773.6,268.8c0,0-5.4-3.5-14.2-1.5M810.6,250.8c0,0-5.4-3.5-14.2-1.5 M850.5,263.5c0,0-7-5-17.1-1.7 M850.5,319.5c0,0-9-3-17-1 M830.5,328.4c6.3,3.6,13.5,4.5,18.9,3.6c3.1-0.5,5.4-1.8,5.4-1.8 M790.5,317.5c4.4,2.2,12.9,2.8,20,1 M766.5,315.5c0,0-8-2-16-1 M748.5,322.5c9.4,5.3,22.2,1.8,20.5,1.8 M873.5,448.5c2,10,8,13,8,13 M849.7,482.9c0,0-15.2-0.4-26.2,10.6 M840.5,464.5c6,5,14.1,6.7,14.1,6.7M694.5,452.5c27-4,46.9-0.4,85-9c31-7,41-24,41-24 M814.5,455.5c-48.4,13.1-60,48-68,80';
 var theirhand = new CompoundPath(pathData1);
 var yourhand = new CompoundPath(pathData2);
-theirhand.fillColor = '#ffd5b3';
-yourhand.fillColor = '#775c47';
+theirhand.strokeColor = 'white';
+theirhand.fillColor = 'black';
+theirhand.strokeWidth = 2;
+theirhand.strokeCap = 'round';
+yourhand.strokeColor = 'white';
+yourhand.fillColor = 'black';
+yourhand.strokeWidth = 2;
+yourhand.strokeCap = 'round';
 theirhand.position = view.center;
 yourhand.position = view.center;
-theirhand.scale(3);
-yourhand.scale(3);
+yourhand.scale(0.6);
+theirhand.scale(0.6);
 
+var points = 6;
+
+var length = 100;
+
+var path1 = new Path({
+	strokeColor: 'white',
+	strokeWidth: 2,
+	strokeCap: 'round'
+});
+
+var path2 = new Path({
+	strokeColor: 'white',
+	strokeWidth: 2,
+	strokeCap: 'round'
+});
+
+var start1 = new Point(-500, 500);
+for (var i = 0; i < points; i++)
+	path1.add(start1 + new Point(i * length, 0));
+
+var start2 = new Point(-500, 580);
+for (var i = 0; i < points; i++)
+	path2.add(start2 + new Point(i * length, 0));
 
 function onMouseMove(event) {
 	yourhand.position = event.point;
-    showIntersections(yourhand, theirhand);
     changeBackground(yourhand, theirhand);
+    
+    path1.firstSegment.point = event.point + new Point(-82, 114);
+	for (var i = 0; i < points - 2; i++) {
+		var segment = path1.segments[i];
+		var nextSegment = segment.next;
+		var vector = segment.point - nextSegment.point;
+		vector.length = length;
+		nextSegment.point = segment.point - vector;
+	}
+	path1.smooth({ type: 'continuous' });
+    
+        path2.firstSegment.point = event.point + new Point(0, 140);
+	for (var i = 0; i < points - 2; i++) {
+		var segment = path2.segments[i];
+		var nextSegment = segment.next;
+		var vector = segment.point - nextSegment.point;
+		vector.length = length;
+		nextSegment.point = segment.point - vector;
+	}
+	path2.smooth({ type: 'continuous' });
 }
 
 function changeBackground(path1, path2){
@@ -26,17 +77,6 @@ function changeBackground(path1, path2){
     if (intersections.length > 10) {
         document.body.style.backgroundColor = "red";
     } else {
-        document.body.style.backgroundColor = "white";
-    }
-}
-
-function showIntersections(path1, path2) {
-    var intersections = path1.getIntersections(path2);
-    for (var i = 0; i < intersections.length; i++) {
-        new Path.Circle({
-            center: intersections[i].point,
-            radius: 5,
-            fillColor: '#009dec'
-        }).removeOnMove();
+        document.body.style.backgroundColor = "black";
     }
 }
